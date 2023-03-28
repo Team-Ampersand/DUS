faa5b30e19c944a63d340e54644c68231cebbab2
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-03-28 19:43:54 +0900

    :sparkles: :: color(dark, light) 추가

---
+++ dotori-components/src/main/java/com/dotori/dotori_components/theme/DotoriColor.kt
@@ -0,0 +1,50 @@
+package com.dotori.dotori_components.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
+object DotoriLightColor {
+    val Error = Color(0xFFD84D4D)
+    val Positive = Color(0xFF2E8546)
+
+    val Primary10 = Color(0xFF6F7AEC)
+    val Primary20 = Color(0xFFEEF0FD)
+    val Primary30 = Color(0xFFC5CAF7)
+
+    val Neutral10 = Color(0xFF292E3D)
+    val Natural20 = Color(0xFF656B80)
+    val Natural30 = Color(0xFFBBBBCC)
+    val Natural40 = Color(0xFFE2E2EE)
+    val Natural50 = Color(0xFFF2F2F4)
+
+    val BackGround = Color(0xFFF7F7F9)
+    val CardBackGround = Color(0xFFFDFDFD)
+
+    val SubGreen = Color(0xFF9BDA93)
+    val SubYellow = Color(0xFFEEE170)
+    val SubRed = Color(0xFFF3A199)
+}
+
+object DotoriDarkColor {
+    val Error = Color(0xFFFF7D7D)
+    val Positive = Color(0xFF5EFF8B)
+
+    val Primary10 = Color(0xFF5966E9)
+    val Primary20 = Color(0xFF1D2136)
+    val Primary30 = Color(0xFF24295D)
+
+    val Neutral10 = Color(0xFFCDCDD5)
+    val Natural20 = Color(0xFF818198)
+    val Natural30 = Color(0xFF626274)
+    val Natural40 = Color(0xFF292930)
+    val Natural50 = Color(0xFF1E1E26)
+
+    val BackGround = Color(0xFF1F1E2B)
+    val CardBackGround = Color(0xFF17161F)
+
+    val SubGreen = Color(0xFF79CE6F)
+    val SubYellow = Color(0xFFE9D844)
+    val SubRed = Color(0xFFEE786D)
+}
