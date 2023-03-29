a9a6c117b33093d637997da22b940b25bb8bd518
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-03-29 22:19:05 +0900

    :sparkles: :: component: button 추가

---
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/utils/Types.kt
@@ -0,0 +1,16 @@
+package com.dotori.dotori_components.components.utils
+
+object Types {
+    enum class ButtonType {
+        LIGHTBLUE,
+        BLUE,
+        DARKBLUE,
+        TRANSPARENT
+    }
+
+    enum class LongButtonType {
+        LIGHTBLUE,
+        BLUE,
+        DARKBLUE
+    }
+}
