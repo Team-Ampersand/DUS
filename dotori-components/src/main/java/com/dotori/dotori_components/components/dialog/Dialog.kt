c12a8cbccdcf2900bca2f57a6af3b57448d8d01a
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2023-04-03 20:43:47 +0900

    :lipstick: Component DotoriColor(Theme) 적용하기

--- dotori-components/src/main/java/com/dotori/dotori_components/components/dialog/Dialog.kt
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/dialog/Dialog.kt
@@ -7,21 +7,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import com.dotori.dotori_components.components.utils.Types.DialogType
-import com.dotori.dotori_components.theme.DotoriDarkColor
-import com.dotori.dotori_components.theme.DotoriLightColor
+import com.dotori.dotori_components.theme.DotoriColor.CardBackGround
 
 @Composable
 fun DotoriDialog(
     modifier: Modifier = Modifier,
-    type: DialogType,
     onDismiss: () -> Unit,
     content: @Composable BoxScope.() -> Unit,
 ) {
-    val color = backgroundColorFor(type)
+    val color = CardBackGround
 
     Dialog(
         onDismissRequest = { onDismiss() }
@@ -38,10 +34,3 @@ fun DotoriDialog(
         }
     }
 }
-
-@Composable
-private fun backgroundColorFor(type: DialogType): Color =
-    when (type) {
-        DialogType.LIGHT -> DotoriLightColor.CardBackGround
-        DialogType.DARK -> DotoriDarkColor.CardBackGround
-    }
