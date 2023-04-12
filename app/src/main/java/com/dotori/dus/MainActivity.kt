0c7554d771536acc13bb841f4631503ff0feb885
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-12 17:29:10 +0900

    :lipstick: :: theme 변경 switch component 생성

--- app/src/main/java/com/dotori/dus/MainActivity.kt
+++ app/src/main/java/com/dotori/dus/MainActivity.kt
@@ -1,29 +1,47 @@
 package com.dotori.dus
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.dotori.dotori_components.theme.DotoriTypography
-import com.dotori.dus.ui.theme.DUSTheme
+import androidx.compose.ui.unit.dp
+import com.dotori.dotori_components.components.toggle.DotoriThemeSwitchButton
+import com.dotori.dotori_components.theme.DotoriColor
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            // 시스템 설정에 따라 앱의 테마를 설정합니다.
+            val checkDarkMode = if (isSystemInDarkTheme())  // 현재 디바이스가 어두운 테마인지 확인합니다.
+                AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+            AppCompatDelegate.setDefaultNightMode(checkDarkMode)
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(DotoriColor.BackGround)
+            ) {
+                DotoriThemeSwitchButton(
+                    switchPadding = 5.dp,
+                    buttonWidth = 70.dp,
+                    buttonHeight = 40.dp,
+                    isChecked = false,
+                    onSwitchClick = {
+                        Log.d("TAG", "onCreate clicked")
+                    }
+                )
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun Preview() {
 }
---
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/toggle/Switch.kt
@@ -0,0 +1,80 @@
+package com.dotori.dotori_components.components.toggle
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.dotori.dotori_components.theme.DarkSwitchIcon
+import com.dotori.dotori_components.theme.DotoriColor
+import com.dotori.dotori_components.theme.LightSwitchIcon
+
+@Composable
+fun DotoriThemeSwitchButton(
+    switchPadding: Dp,
+    buttonWidth: Dp,
+    buttonHeight: Dp,
+    isChecked: Boolean,
+    lightIcon: @Composable () -> Unit = { LightSwitchIcon(contentDescription = "light switch") },
+    darkIcon: @Composable () -> Unit = { DarkSwitchIcon(contentDescription = "dark switch") },
+    onSwitchClick: () -> Unit
+) {
+    val switchSize by remember { mutableStateOf(buttonHeight-switchPadding*2) }
+    val interactionSource = remember { MutableInteractionSource() }
+    var switchClicked by remember { mutableStateOf(isChecked) }
+    var padding by remember { mutableStateOf(0.dp) }
+
+    padding = if (switchClicked) buttonWidth-switchSize-switchPadding*2 else 0.dp
+
+    val animateSize by animateDpAsState(
+        targetValue = if (switchClicked) padding else 0.dp,
+        tween(
+            durationMillis = 500,
+            delayMillis = 0,
+            easing = LinearOutSlowInEasing
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .width(buttonWidth)
+            .height(buttonHeight)
+            .clip(CircleShape)
+            .background(if (switchClicked) DotoriColor.Black else DotoriColor.Natural40)
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null
+            ) {
+                switchClicked = !switchClicked
+                onSwitchClick()
+            }
+    ) {
+        Row(modifier = Modifier
+            .fillMaxSize()
+            .padding(switchPadding)
+        ) {
+             Box(
+                 modifier = Modifier
+                     .fillMaxHeight()
+                     .width(animateSize)
+             )
+
+            Box(
+                modifier = Modifier
+                    .size(switchSize)
+                    .clip(CircleShape)
+            ) {
+                if (switchClicked) darkIcon()
+                else lightIcon()
+            }
+        }
+    }
+}
