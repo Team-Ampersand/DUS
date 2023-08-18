8fd5365585a24eb740dd76030c047074f56f25c1
박성현 <pjhomg5530@naver.com>
2023-08-18 11:12:31 +0900

    :fire: :: 안 쓰는 코드 지우기

--- dotori-components/src/main/java/com/dotori/dotori_components/components/button/SegmentedButtons.kt
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/button/SegmentedButtons.kt
@@ -6,14 +6,13 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,12 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.dotori.dotori_components.theme.DotoriTheme
-import com.dotori.dotori_components.theme.Transparent
 
 @Composable
 fun DotoriSegmentedButtons(
@@ -48,11 +45,7 @@ fun DotoriSegmentedButtons(
                 shape = RoundedCornerShape(outRoundedCornerShape)
             ),
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(rowPadding),
-            ) {
+            Row(modifier = Modifier.padding(rowPadding)) {
                 list.forEachIndexed { index, text ->
                     Box(
                         modifier = Modifier
@@ -88,9 +81,25 @@ fun DotoriSegmentedButtons(
 @Composable
 fun preview() {
     var list = mutableListOf("남", "여")
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(DotoriTheme.colors.background)) {
+    var list1 = mutableListOf("아침", "점심", "저녁")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DotoriTheme.colors.background)
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        DotoriSegmentedButtons(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            textPadding = 10.dp,
+            rowPadding = 6.dp,
+            outRoundedCornerShape = 8.dp,
+            innerRoundedCornerShape = 4.dp,
+            list = list1,
+            onSwitchClick = {}
+        )
+        Spacer(modifier = Modifier.height(20.dp))
         DotoriSegmentedButtons(
             modifier = Modifier
                 .fillMaxWidth()
