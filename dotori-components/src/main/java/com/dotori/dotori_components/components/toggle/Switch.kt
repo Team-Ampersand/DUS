097ed1334e6cdbbcddf0ff490b27c53b2b1ab834
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-08-21 19:53:28 +0900

    :recycle: :: switch theme에 따라 변경되게 수정

--- dotori-components/src/main/java/com/dotori/dotori_components/components/toggle/Switch.kt
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/toggle/Switch.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.dotori.dotori_components.theme.Black
 import com.dotori.dotori_components.theme.DotoriTheme
 import com.dotori.dotori_components.theme.White
 
@@ -81,7 +82,7 @@ fun DotoriSwitchButton(
             Box(
                 modifier = Modifier.size(24.dp)
                     .clip(CircleShape)
-                    .background(White),
+                    .background(if (DotoriTheme.isSystemIsDarkTheme()) Black else White),
             )
         }
     }
