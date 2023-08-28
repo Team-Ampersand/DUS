a1adf980f6ebb828a58b7abeb7c8f1ebbf148825
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-08-28 13:53:40 +0900

    :fire: :: dotori long button 삭제

--- dotori-components/src/main/java/com/dotori/dotori_components/components/button/DotoriButton.kt
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/button/DotoriButton.kt
@@ -1,7 +1,6 @@
 package com.dotori.dotori_components.components.button
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -11,9 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.dotori.dotori_components.components.utils.Types
 import com.dotori.dotori_components.theme.DotoriTheme
 import com.dotori.dotori_components.theme.Transparent
 import com.dotori.dotori_components.theme.White
@@ -27,64 +24,36 @@ fun DotoriButton(
 ) {
     if (colors == Transparent) {
         OutlinedButton(
-            onClick = onClick,
             modifier = modifier,
             border = BorderStroke(1.dp, DotoriTheme.colors.neutral30),
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Transparent,
                 contentColor = DotoriTheme.colors.neutral20
             ),
-            shape = RoundedCornerShape(8.dp)
+            shape = RoundedCornerShape(8.dp),
+            onClick = onClick
         ) {
             Text(
                 modifier = Modifier.padding(horizontal = 15.dp),
                 text = text,
-                color = DotoriTheme.colors.neutral50,
                 style = DotoriTheme.typography.smallTitle
             )
         }
     } else {
         Button(
-            onClick = onClick,
             modifier = modifier,
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = colors,
                 contentColor = White
             ),
-            shape = RoundedCornerShape(8.dp)
+            shape = RoundedCornerShape(8.dp),
+            onClick = onClick
         ) {
             Text(
                 modifier = Modifier.padding(horizontal = 15.dp),
                 text = text,
-                color = DotoriTheme.colors.neutral50,
                 style = DotoriTheme.typography.smallTitle
             )
         }
     }
 }
-
-@Composable
-fun DotoriLongButton(
-    text: String,
-    colors: Types.LongButtonType,
-    horizontalPaddingValue: Dp = 10.dp,
-    onClick: () -> Unit
-) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = horizontalPaddingValue, end = horizontalPaddingValue),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = when (colors) {
-                Types.LongButtonType.BLUE -> DotoriTheme.colors.primary10
-                Types.LongButtonType.LIGHTBLUE -> DotoriTheme.colors.primary30
-            },
-            contentColor = White
-        ),
-        shape = RoundedCornerShape(8.dp),
-    ) {
-        Text(text = text)
-    }
-}
-
