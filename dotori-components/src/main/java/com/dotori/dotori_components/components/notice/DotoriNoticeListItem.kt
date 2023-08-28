d8fea1d88252147caaa414a5265a739df1f41fd7
박성현 <pjhomg5530@naver.com>
2023-08-28 15:51:34 +0900

    :recycle: :: enum class를 사용해서 리팩토링

--- dotori-components/src/main/java/com/dotori/dotori_components/components/notice/DotoriNoticeListItem.kt
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/notice/DotoriNoticeListItem.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dotori.dotori_components.components.utils.RoleType
 import com.dotori.dotori_components.theme.DotoriTheme
 import com.dotori.dotori_components.theme.White
 
@@ -39,6 +40,7 @@ fun DotoriNoticeListItem(
     title: String,
     content: String,
     date: String,
+    role: String,
     background: Color = DotoriTheme.colors.neutral50,
     focusColor: Color = DotoriTheme.colors.primary10
 ) {
@@ -71,9 +73,9 @@ fun DotoriNoticeListItem(
                 modifier = Modifier
                     .size(12.dp)
                     .background(
-                        color = when (writer) {
-                            "도토리" -> DotoriTheme.colors.primary10
-                            "사감 선생님" -> DotoriTheme.colors.subYellow
+                        color = when (role) {
+                            RoleType.ROLE_DEVELOPER.toString() -> DotoriTheme.colors.primary10
+                            RoleType.ROLE_ADMIN.toString() -> DotoriTheme.colors.subYellow
                             else -> DotoriTheme.colors.subRed
                         },
                         shape = CircleShape
@@ -125,21 +127,24 @@ fun DotoriNoticeListItemPreview() {
                 writer = "도토리",
                 title = "[기숙사 자습실 사용 관련 공지]",
                 content = "많은 분들이 급식의 화살표를 눌렀을때 날짜만 변경되는 점이 불편하다고 하여 이제는 급식",
-                date = "2023.08.28"
+                date = "2023.08.28",
+                role = "ROLE_DEVELOPER"
             )
             DotoriNoticeListItem(
                 modifier = Modifier.fillMaxWidth(),
                 writer = "사감 선생님",
                 title = "[기숙사 자습실 사용 관련 공지]",
                 content = "운전모드를 반드시 냉방으로 맞춰야 합니다. 만약 난방으로 맞추고 가동할 경우 에어컨 가동이 중단 될 수 있습니다. 냉방 모드 설정 방법 - 디스플레이 하단에 조절 버튼 중 좌측 상단에 있는 버튼(Auto)을 사용하여 눈송이 표시가 나타나게 설정합니다. 리모컨에 잠금이 걸려있는 경우 - 온도 조절 버튼 2개를 동시에 누르면 잠금이 해제가 됩니다.",
-                date = "2023.08.28"
+                date = "2023.08.28",
+                role = "ROLE_ADMIN"
             )
             DotoriNoticeListItem(
                 modifier = Modifier.fillMaxWidth(),
                 writer = "기숙사자치위원회",
                 title = "[기숙사 자습실 사용 관련 공지]",
                 content = "최근 자습 신청할 때 일학년이 매크로를 사용한다는 이야기가 많아 6월 3일까지 일학년 전체 자습금지 하겠습니다. 매크로를 사용한 학생은 이시완#7244 에 개인 디코 하시길 바랍니다. 또한 자습 신청을 한 후, 다른 학생에게 양도하는 행위 또한 자제해 주시길 바랍니다.",
-                date = "2023.08.28"
+                date = "2023.08.28",
+                role = "ROLE_COUNCILLOR"
             )
         }
     }
