d2253f83b54b17c9cd824bf59192b733f13b1edb
hyeonseo <s21035@gsm.hs.kr>
2023-08-31 16:54:06 +0900

    :wrench: :: maven gradle plugin & release setting

--- dotori-components/build.gradle.kts
+++ dotori-components/build.gradle.kts
@@ -3,6 +3,20 @@ plugins {
     id(Plugins.kotlinAndroid)
     id(Plugins.kotlinKapt)
     id(Plugins.kotlinParcelize)
+    id(Plugins.mavenPublish)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register("release", MavenPublication::class) {
+                from(components["release"])
+                version = Versions.COMPONENT_VERSION
+                artifactId = ProjectProperties.ArtifactId
+                groupId = ProjectProperties.GroupId
+            }
+        }
+    }
 }
 
 android {
