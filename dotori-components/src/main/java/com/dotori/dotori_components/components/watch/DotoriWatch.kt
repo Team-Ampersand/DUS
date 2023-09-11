c6600559a6e2cf4a0b744036315dfb1a6649df9e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-09-11 15:36:36 +0900

    🔥 :: Remove not used import

--- dotori-components/src/main/java/com/dotori/dotori_components/components/watch/DotoriWatch.kt
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/watch/DotoriWatch.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
@@ -28,9 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dotori.dotori_components.theme.DotoriTheme
 import com.example.dus.R
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 
