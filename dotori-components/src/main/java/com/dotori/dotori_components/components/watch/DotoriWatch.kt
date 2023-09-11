8c44f0c5e448dc780e4df9d233f8eaa78a0af48a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-09-11 16:45:10 +0900

    ✨:: Change the height from limit to reactive phone size

--- dotori-components/src/main/java/com/dotori/dotori_components/components/watch/DotoriWatch.kt
+++ dotori-components/src/main/java/com/dotori/dotori_components/components/watch/DotoriWatch.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,9 +20,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,11 +41,16 @@ fun DotoriWatch(
     modifier: Modifier = Modifier,
     time: Date,
 ) {
+    val boxHeight = remember {
+        mutableStateOf(100.dp)
+    }
+    val density = LocalDensity.current
     Box(modifier = modifier) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(100.dp)
+                .height(boxHeight.value)
+                .clip(RoundedCornerShape(16.dp))
                 .paint(
                     painter = painterResource(id = R.drawable.bg_dotori_watch),
                     contentScale = ContentScale.Crop
@@ -58,12 +67,18 @@ fun DotoriWatch(
             )
         }
         Column(
-            modifier = Modifier.padding(top = 24.dp, start = 24.dp),
+            modifier = Modifier
+                .padding(top = 24.dp, start = 24.dp, bottom = 24.dp)
+                .onSizeChanged { layoutCoordinates ->
+                    with(density) {
+                        boxHeight.value = layoutCoordinates.height.toDp() + 54.dp
+                    }
+                },
             verticalArrangement = Arrangement.spacedBy(6.dp)
         ) {
             Text(
                 text = "현재시간",
-                style = DotoriTheme.typography.smallTitle,
+                style = DotoriTheme.typography.caption,
                 color = Color.White
             )
             Row(
